ae6267d59ad669cc6eeeeedab8e02443063312f3
tuancuongbota <93958224+tuancuongbota@users.noreply.github.com>
2023-09-22 09:33:30 +0700

    cuongnt

--- statics/scripts/js_custom.js
+++ statics/scripts/js_custom.js
@@ -46,4 +46,87 @@ $(document).ready(function(){
         });
     });
  }
+ function isotopeArrange() {
+    $(".grid-layout > .isotope").find("audio, video").each(function (index) {
+       $(this)[0].play();
+       $(this)[0].pause()
+    })
+ }
+ if ($(".grid-layout > .isotope").length) {
+    $(".grid-layout > .isotope").each(function () {
+       var c_elem = $(this);
+       var parent_width = c_elem.width();
+       var gutter_size = c_elem.data("gutter");
+       var grid_cols = c_elem.data("cols");
+       var layoutmode = c_elem.is('[data-layout]') ? c_elem.data("layout") : '';
+       layoutmode = layoutmode ? layoutmode : 'masonry';
+       if ($(window).width() < 768) grid_cols = 1;
+       var net_width = Math.floor((parent_width - (gutter_size * (grid_cols - 1))) / grid_cols);
+       c_elem.find("article").css({
+          'width': net_width + 'px',
+          'margin-bottom': gutter_size + 'px'
+       });
+       c_elem.find(".owl-carousel").each(function () {
+          factrieOwlSettings($(this))
+       });
+       c_elem.find("audio").each(function (index) {});
+       c_elem.find("video").each(function (index) {
+          $(this).attr("src", $(this).find("source").attr("src"));
+          $(this).css({
+             'height': '200px'
+          })
+       });
+       var filter = "*";
+       var isot_parent = c_elem.parent(".grid-layout");
+       if ($(isot_parent).attr("data-filter-stat") == 0) {
+          filter = $(isot_parent).attr("data-first-cat") ? "." + $(isot_parent).attr("data-first-cat") : '*'
+       }
+       c_elem.imagesLoaded(function () {
+          c_elem.isotope({
+             itemSelector: 'article',
+             layoutMode: layoutmode,
+             filter: filter,
+             masonry: {
+                gutter: gutter_size
+             },
+             fitRows: {
+                gutter: gutter_size
+             }
+          })
+       });
+       c_elem.children("article").addClass("grid-visible");
+       if ($(".portfolio-filter").length) {
+          $(".portfolio-filter-item").on('click', function () {
+             $(this).parents("ul.nav").find("li").removeClass("active");
+             $(this).parent("li").addClass("active");
+             var filterValue = $(this).attr("data-filter");
+             c_elem = $(this).parents(".portfolio-wrapper").find(".grid-layout .isotope");
+             c_elem.isotope({
+                filter: filterValue
+             });
+             return !1
+          })
+       }
+       $(window).resize(function () {
+          setTimeout(function () {
+             grid_cols = c_elem.data("cols");
+             if ($(window).width() < 768) grid_cols = 1;
+             var parent_width = c_elem.width();
+             net_width = Math.floor((parent_width - (gutter_size * (grid_cols - 1))) / grid_cols);
+             c_elem.find("article").css({
+                'width': net_width + 'px'
+             });
+             c_elem.imagesLoaded(function () {
+                var $isot = c_elem.isotope({
+                   itemSelector: 'article',
+                   masonry: {
+                      gutter: gutter_size
+                   }
+                });
+                $isot.on('arrangeComplete', isotopeArrange)
+             })
+          }, 200)
+       });
+    })
+ }
 });
